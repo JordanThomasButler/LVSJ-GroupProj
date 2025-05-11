1f9a1a2154da4f204a82e457d70d3a5f25ebe7f6
JordanThomasButler <106013677@swin.edu.au>
2025-05-11 18:41:27 +1000

    updating include files

--- project1/README.txt
+++ project1/README.txt
@@ -1,3 +1,4 @@
 Color Scheme:
 https://ca.pinterest.com/pin/720083427952069829/
 
+Raw umber: #8A6240
--- project1/template.php
+++ project1/template.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Template File</title>
-    <link rel="stylesheet" href="../styles/styles.css">
-</head>
-<body class="template">
-    <?php
-    
-    ?>
-</body>
-</html>
+<?php
+    include("header.inc");
+?>
+
+<?php
+    include("footer.inc");
+?>
